503956a3a54f5d43f809089ff5b853375d0119bd
ABHIRUP <abhirupsaha02@gmail.com>
2022-06-03 15:06:42 +0530

    update

---
+++ binary_search.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//array must be sorted before we can apply binary search
+
+int binarySerach(int arr[], int n, int key) {
+    int s = 0;
+    int e = n;
+    while (s <= e) {
+        int mid = (s + e) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        else if (arr[mid] > key) {
+            e = mid - 1;
+        }
+        else {
+            s = mid + 1;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter array size: ";
+    cin>>n;
+    
+    int a[n];
+    cout <<"Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int key;
+    cout << "Enter the key: ";
+    cin >> key;
+    cout << binarySerach(a, n, key);
+   
+}
---
+++ sum_of_subarrays.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    int curr_sum = 0;
+    for(int i=0; i<n; i++){
+        curr_sum =0;
+        for(int j=i; j<n; j++){
+            curr_sum+=a[j];
+            cout<<curr_sum<<endl;
+        }
+    }
+}
