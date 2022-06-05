f9af294c2c12fdb432b1da3a62e80a64ce25d381
ABHIRUP <abhirupsaha02@gmail.com>
2022-06-05 12:52:49 +0530

    update

---
+++ max_till_i.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int mx = INT_MIN;
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    for(int i=0; i<n; i++){
+        mx=max(mx,a[i]);
+        cout<<mx<<endl;
+    }
+
+
+
+}
