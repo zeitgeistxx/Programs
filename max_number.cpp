a89007e29b341bd05a9c0a3606103a4c35a268f0
ABHIRUP <abhirupsaha02@gmail.com>
2022-06-07 00:30:15 +0530

    update

---
+++ max_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,max;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    /*
+      if a>b and a>c, then we assign a 
+      to max else if b>c we assign b 
+      to max else we assign c to max
+    */
+
+    max = a>b && a>c ? a : b>c ? b : c;
+
+    cout << "The maximum number is "<<max << endl;
+
+}
