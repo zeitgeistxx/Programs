fac298e488d44453f7ef768c0cc477febfb6986e
ABHIRUP <abhirupsaha02@gmail.com>
2022-07-14 21:52:01 +0530

    update

---
+++ PascalTriangle.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void PascalTriangle(int n){
+    for(int i = 0; i < n; i++){
+        int space = 40 - 3 * i;
+        int bin = 1;
+        for(int j = 1; j <= space; j++){
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++){
+            if(i==0||k==0){
+                bin = 1;
+                cout << bin;
+            }
+            else{
+                bin = bin * (i - k + 1) / k;
+                cout << setw(6) << bin;
+            }
+        }
+        cout << endl;
+    }
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    PascalTriangle(n);
+}
