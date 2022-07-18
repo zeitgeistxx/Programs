ca813baa6ca0c375aab621e902712fb758bc9e95
ABHIRUP <abhirupsaha02@gmail.com>
2022-07-18 21:35:18 +0530

    update

---
+++ largestwordinSentence.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    //Largest word in a sentence
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    char arr[n + 1];
+    cin.getline(arr, n);
+    cin.ignore();//to clear the buffer
+
+    int i = 0;
+    int curLen = 0, maxLen = 0;
+    int st = 0, maxst = 0;
+    while(1){
+
+        if(arr[i]==' ' || arr[i]=='\0'){
+            if(curLen>maxLen){
+                maxLen = curLen;
+                maxst = st;
+            }
+
+            curLen = 0;
+            st = i + 1;
+        }
+
+        else{
+            curLen++;
+        }
+
+        if(arr[i]=='\0') break;
+
+        i++;
+    }
+    cout << maxLen << endl;
+    for(int i =0; i<maxLen; i++){
+        cout << arr[i + maxst];
+    }
+}
+
+
---
+++ pointer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#define size(x) sizeof(x)/sizeof(x[0])
+
+int main(){
+    int n = 20;
+    int *aptr;
+
+    aptr = &n;
+    cout << *aptr << endl;
+    *aptr=21;
+    cout << *aptr << endl;
+
+    int arr[]{10, 20, 30, 40};
+    cout << *arr << endl;
+
+    int *ptr = arr;
+    for (int i = 0; i < size(arr); i++){
+        //cout << *(arr + i) << " "; //index pointer
+        // arr++; // this is constant , cannot be incremented
+        cout << *ptr <<" ";
+        ptr++;
+    }
+}
+
