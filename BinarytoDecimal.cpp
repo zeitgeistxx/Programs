de35c07795f8e18bdac8ab7168fe6f03dcecd6cd
ABHIRUP <abhirupsaha02@gmail.com>
2022-07-21 19:45:55 +0530

    update

---
+++ BinarytoDecimal.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int BinarytoDecimal(string s){
+    int sum = 0;
+    for(int i=s.length()-1; i>=0; i--){
+        if(s[s.length()-i-1]=='0'){
+            sum += 0 * pow(2, i);
+        }
+        else{
+            sum += 1 * pow(2, i);
+        }
+
+    }
+    return sum;
+}
+
+
+int main(){
+    string s;
+    cin >> s;
+
+    cout << BinarytoDecimal(s) << endl;
+}
