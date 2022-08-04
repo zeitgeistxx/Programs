01757e47ae318c29937b877447dc49149a00bed4
ABHIRUP <abhirupsaha02@gmail.com>
2022-08-04 18:58:54 +0530

    update

--- stack.cpp
+++ stack.cpp
@@ -1,61 +1,139 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#define size 10
 
-struct ArrayStack{
+class Stack
+{
+    int arr[size];
     int top;
-    int capacity;
-    int *array;
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+    bool isFull();
+    bool isEmpty();
+    void push(int x);
+    void pop();
+    int peek();
+    void display();
 };
 
-struct ArrayStack *CreateStack(){
-    struct ArrayStack *S = malloc(sizeof(struct ArrayStack));
-
-    if(!S){
-        return null;
+bool Stack::isFull()
+{
+    if (top == size - 1)
+    {
+        return true;
     }
-    S->capacity = 1;
-    s->top = -1;
-    S->array = malloc(S->capacity * sizeof(int));
-
-    if(!S->array){
-        return null;
+    return false;
+}
+bool Stack::isEmpty()
+{
+    if (top == -1)
+    {
+        return true;
     }
-    return S;
+    return false;
 }
 
-int IsEmptyStack(struct ArrayStack *S){
-    return (S->top == -1);
-}
-int IsFullStack(struct ArrayStack *S){
-    return (S->top == S->capacity - 1);
-}
-void Push(struct ArrayStack *S,int data){
-    if(isFullStack(S)){
-        cout << "Stack Overflow" << endl;
+void Stack::push(int x)
+{
+    if (isFull())
+    {
+        cout << "Stack is Full" << endl;
     }
-    else{
-        S->array[++S->top] = data;
-    }
+    top++;
+    arr[top] = x;
 }
-int Pop(struct ArrayStack *S){
-    if(IsEmptyStack(S)){
+
+void Stack::pop()
+{
+    if (isEmpty())
+    {
         cout << "Stack is empty" << endl;
-        return 0;
     }
-    else{
-        return (S->array[S->top--]);
-    }
+    int x = arr[top];
+    top--;
 }
 
-void DeleteStack(struct DynArrayStack *S){
-    if(S){
-        if(S->array){
-            free(S->array);
+int Stack::peek()
+{
+    if (isEmpty())
+    {
+        cout << "Stack Empty" << endl;
+    }
+    int x = arr[top];
+    return x;
+}
+
+void Stack::display()
+{
+    if (!isEmpty())
+    {
+        for (int i = 0; i <= top; i++)
+        {
+            cout << arr[i] << " ";
         }
-        free(S);
     }
+    cout << "\n\n\n";
 }
 
-int main(){
+int main()
+{
+    class Stack s;
+    int item;
 
+    while (true)
+    {
+        cout << "Press 1 to check if Stack is Full" << endl;
+        cout << "Press 2 to check if Stack is Empty" << endl;
+        cout << "Press 3 to push an item to Stack" << endl;
+        cout << "Press 4 to pop top item from Stack" << endl;
+        cout << "Press 5 to peek into Stack" << endl;
+        cout << "Press 6 to Display the Stack" << endl;
+        cout << "Choose an operation ->" << endl;
+
+        int y;
+        cin >> y;
+        switch (y)
+        {
+        case 1:
+            if (s.isFull() == 0)
+                cout << "False" << endl;
+            else
+                cout << "True" << endl;
+            break;
+
+        case 2:
+            if (s.isEmpty() == 0)
+                cout << "False" << endl;
+            else
+                cout << "True" << endl;
+            break;
+
+        case 3:
+            cout << "Enter an item to push :";
+            cin >> item;
+            s.push(item);
+            break;
+
+        case 4:
+            s.pop();
+            break;
+
+        case 5:
+            cout << "The top element " << s.peek() << endl;
+            break;
+
+        case 6:
+            cout << "The Stack ->";
+            s.display();
+            break;
+
+        default:
+            cout << "Invalid Operation" << endl;
+            exit(0);
+        }
+    }
 }
