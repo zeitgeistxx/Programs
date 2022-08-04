d4782b260f97140faa603cd4a54ff81f1d2b3e12
ABHIRUP <abhirupsaha02@gmail.com>
2022-08-04 19:39:13 +0530

    Update

---
+++ balance.cpp
@@ -0,0 +1,50 @@
+//check for balanced brackets.
+
+#include <iostream>
+#include<stack>
+using namespace std;
+
+bool areBracketsBalanced(string expr)
+{
+	stack<char> temp;
+		for(int i=0;i<expr.length();i++)
+		{
+			if(temp.empty())
+			{
+				temp.push(expr[i]);
+			}
+			else if((temp.top()=='('&& expr[i]==')') || (temp.top()=='{' && expr[i]=='}') || (temp.top()=='[' && expr[i]==']'))
+			{
+				temp.pop();
+			}
+			else
+			{
+				temp.push(expr[i]);
+			}
+		}
+        if(temp.empty())
+		{
+			return true;
+		}
+		return false;
+
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    string expr;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(' || s[i]==')' || s[i]=='{' || s[i]=='}' || s[i]=='[' || s[i]==']'){
+            expr.push_back(s[i]);
+        }
+    }
+
+    cout << expr << " -> ";
+    if (areBracketsBalanced(expr))
+		cout << " Balanced";
+	else
+		cout << " Not Balanced";
+
+}
