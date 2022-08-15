9c50a5f70c7c2c57d344fa49606a5050a27cdf99
ABHIRUP <abhirupsaha02@gmail.com>
2022-08-15 14:18:09 +0530

    Update

---
+++ Tail-recursion-Factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n, int prod){
+    if(n==0 || n==1)
+        return prod;
+    return fact(n-1,n*prod);
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fact(n, 1) << endl;
+}
---
+++ Tail-recursion-Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n, int a, int b){
+    if(n==0) return a;
+    if(n==1) return b;
+    return fib(n - 1, b, a + b);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cout << fib(i, 0, 1) << " ";
+    }
+}
