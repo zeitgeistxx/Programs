c1df208a6c5d7e85f4f8bb11cfa3a98e2787663d
ABHIRUP <abhirupsaha02@gmail.com>
2022-10-15 04:00:59 +0530

    modified

--- Security-Project.cpp
+++ Security-Project.cpp
@@ -7,87 +7,105 @@ using namespace std;
 int ascii = 178;
 char ch = ascii;
 
-#define heading \
-    cout << " ";\
-    for (int i = 0; i < 25; i++) cout << ch;\
-    cout << " Security Project ";\
-    for (int i = 0; i < 25; i++) cout << ch;
+#define heading                   \
+    cout << " ";                  \
+    for (int i = 0; i < 25; i++)  \
+        cout << ch;               \
+    cout << " Security Project "; \
+    for (int i = 0; i < 25; i++)  \
+        cout << ch;
 
-#define registering \
-    cout << " ";\
-    for (int i = 0; i < 25; i++) cout << ch;\
-    cout << " REGISTER YOUR ACCOUNT ";\
-    for (int i = 0; i < 25; i++) cout << ch;
+#define registering                    \
+    cout << " ";                       \
+    for (int i = 0; i < 25; i++)       \
+        cout << ch;                    \
+    cout << " REGISTER YOUR ACCOUNT "; \
+    for (int i = 0; i < 25; i++)       \
+        cout << ch;
 
-#define loggingIn \
-    cout << " ";\
-    for (int i = 0; i < 25; i++) cout << ch;\
-    cout << " LOG INTO YOUR ACCOUNT ";\
-    for (int i = 0; i < 25; i++) cout << ch;
+#define loggingIn                      \
+    cout << " ";                       \
+    for (int i = 0; i < 25; i++)       \
+        cout << ch;                    \
+    cout << " LOG INTO YOUR ACCOUNT "; \
+    for (int i = 0; i < 25; i++)       \
+        cout << ch;
 
-#define forgotemail \
-    cout << " ";\
-    for (int i = 0; i < 25; i++) cout << ch;\
-    cout << " FORGOT EMAIL ID ";\
-    for (int i = 0; i < 25; i++) cout << ch;
+#define forgotemail              \
+    cout << " ";                 \
+    for (int i = 0; i < 25; i++) \
+        cout << ch;              \
+    cout << " FORGOT EMAIL ID "; \
+    for (int i = 0; i < 25; i++) \
+        cout << ch;
 
-#define forgotpassword \
-    cout << " ";\
-    for (int i = 0; i < 25; i++) cout << ch;\
-    cout << " FORGOT PASSWORD ";\
-    for (int i = 0; i < 25; i++) cout << ch;
-
+#define forgotpassword           \
+    cout << " ";                 \
+    for (int i = 0; i < 25; i++) \
+        cout << ch;              \
+    cout << " FORGOT PASSWORD "; \
+    for (int i = 0; i < 25; i++) \
+        cout << ch;
 
 int a = 0;
-struct Register
+class Register
 {
-    string id;
-    string name, email, phone, password;
-}R[25];
+    string id, name, email, phone, password;
 
-void registerAccount(){
+public:
+    void registerAccount();
+    void loginAccount();
+    void forgotEmail();
+    void forgotPassword();
+} R[25];
+
+void Register::registerAccount()
+{
     system("cls");
     registering;
-    cout << "\n\n "<< ch << ch << " ENTER USER ID -> ";
+    cout << "\n\n " << ch << ch << " ENTER USER ID -> ";
     cin >> R[a].id;
-    cout << "\n\n "<< ch << ch << " ENTER USERNAME -> ";
+    cout << "\n\n " << ch << ch << " ENTER USERNAME -> ";
     cin >> R[a].name;
-    cout << "\n\n "<< ch << ch << " ENTER EMAIL ID -> ";
+    cout << "\n\n " << ch << ch << " ENTER EMAIL ID -> ";
     cin >> R[a].email;
-    cout << "\n\n "<< ch << ch << " ENTER PHONE NO. -> ";
+    cout << "\n\n " << ch << ch << " ENTER PHONE NO. -> ";
     cin >> R[a].phone;
     char d;
-    cout << "\n\n "<< ch << ch << " ENTER PASSWORD -> ";
-    while(R[a].password.empty()){
+    cout << "\n\n " << ch << ch << " ENTER PASSWORD -> ";
+    while (R[a].password.empty())
+    {
         do
         {
             d = getch();
-            if(isdigit(d) || isalpha(d) || ispunct(d))
+            if (isdigit(d) || isalpha(d) || ispunct(d))
             {
                 R[a].password.push_back(d);
                 cout << "*";
             }
-            if(d == '\b' && !R[a].password.empty())
+            if (d == '\b' && !R[a].password.empty())
             {
                 R[a].password.pop_back();
                 // cursor moves back 1 position
-                cout << "\b" << " " << "\b";
+                cout << "\b"
+                     << " "
+                     << "\b";
             }
         } while (isdigit(d) || isalpha(d) || ispunct(d) || d == '\b' || d == '\t' || d == 32); // spacebar ASCII value 32
     }
-            
+
     a++;
 
     cout << " \n\n\n " << ch << ch << " ACCOUNT REGISTERED SUCCESSFULLY " << ch << ch << "\n ";
     system("pause");
 }
 
-void loginAccount()
+void Register::loginAccount()
 {
     system("cls");
     loggingIn;
     string t_email, t_password;
-    if(a==0)
+    if (a == 0)
     {
         cout << " \n\n\n " << ch << ch << " NO RECORD FOUND " << ch << ch << "\n ";
         system("pause");
@@ -96,33 +114,35 @@ void loginAccount()
     else
     {
         char d;
-        cout << " \n\n "<< ch << ch << " ENTER EMAIL ID -> ";
+        cout << " \n\n " << ch << ch << " ENTER EMAIL ID -> ";
         cin >> t_email;
-        cout << " \n\n "<< ch << ch << " ENTER PASSWORD -> ";
-        while(t_password.empty()){
+        cout << " \n\n " << ch << ch << " ENTER PASSWORD -> ";
+        while (t_password.empty())
+        {
             do
             {
                 d = getch();
-                if(isdigit(d) || isalpha(d) || ispunct(d))
+                if (isdigit(d) || isalpha(d) || ispunct(d))
                 {
                     t_password.push_back(d);
                     cout << "*";
                 }
-                if(d == '\b' && !R[a].password.empty())
+                if (d == '\b' && !R[a].password.empty())
                 {
                     t_password.pop_back();
-                    cout << "\b" << " " << "\b";
+                    cout << "\b"
+                         << " "
+                         << "\b";
                 }
             } while (isdigit(d) || isalpha(d) || ispunct(d) || d == '\b' || d == '\t' || d == 32);
         }
-
     }
 
     // match email and password
     int found = 0;
     for (int k = 0; k < a; k++)
     {
-        if(t_email == R[k].email && t_password == R[k].password)
+        if (t_email == R[k].email && t_password == R[k].password)
         {
             cout << "\n\n ";
             for (int i = 0; i < 25; i++)
@@ -136,24 +156,24 @@ void loginAccount()
             break;
         }
     }
-    if(found == 0)
+    if (found == 0)
     {
-        cout << " \n\n " << ch << ch << " EMAILID OR PASSWORD IS INCORRECT " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " EMAILID OR PASSWORD IS INCORRECT " << ch << ch << "\n ";
         system("pause");
     }
     else
     {
-        cout << " \n\n " << ch << ch << " LOGGED IN SUCCESSFULLY " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " LOGGED IN SUCCESSFULLY " << ch << ch << "\n ";
         system("pause");
     }
 }
 
-void forgotEmail()
+void Register::forgotEmail()
 {
     system("cls");
     forgotemail;
     string uid, t_password;
-    if(a==0)
+    if (a == 0)
     {
         cout << " \n\n\n " << ch << ch << " NO RECORD FOUND " << ch << ch << "\n ";
         system("pause");
@@ -161,34 +181,36 @@ void forgotEmail()
     }
     else
     {
-        cout << " \n\n "<< ch << ch << " ENTER USERID -> ";
+        cout << " \n\n " << ch << ch << " ENTER USERID -> ";
         cin >> uid;
         char d;
-        cout << " \n\n "<< ch << ch << " ENTER PASSWORD -> ";
-        while(t_password.empty()){
+        cout << " \n\n " << ch << ch << " ENTER PASSWORD -> ";
+        while (t_password.empty())
+        {
             do
             {
                 d = getch();
-                if(isdigit(d) || isalpha(d) || ispunct(d))
+                if (isdigit(d) || isalpha(d) || ispunct(d))
                 {
                     t_password.push_back(d);
                     cout << "*";
                 }
-                if(d == '\b' && !R[a].password.empty())
+                if (d == '\b' && !R[a].password.empty())
                 {
                     t_password.pop_back();
-                    cout << "\b" << " " << "\b";
+                    cout << "\b"
+                         << " "
+                         << "\b";
                 }
             } while (isdigit(d) || isalpha(d) || ispunct(d) || d == '\b' || d == '\t' || d == 32);
         }
-
     }
 
     // match password and userid
     int found = 0;
     for (int k = 0; k < a; k++)
     {
-        if(t_password == R[k].password && uid == R[k].id)
+        if (t_password == R[k].password && uid == R[k].id)
         {
             cout << "\n\n ";
             for (int i = 0; i < 25; i++)
@@ -202,24 +224,24 @@ void forgotEmail()
             break;
         }
     }
-    if(found == 0)
+    if (found == 0)
     {
-        cout << " \n\n " << ch << ch << " USER DATA INCORRECT " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " USER DATA INCORRECT " << ch << ch << "\n ";
         system("pause");
     }
     else
     {
-        cout << " \n\n " << ch << ch << " INFORMATION RETRIEVED SUCCESSFULLY " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " INFORMATION RETRIEVED SUCCESSFULLY " << ch << ch << "\n ";
         system("pause");
     }
 }
 
-void forgotPassword()
+void Register::forgotPassword()
 {
     system("cls");
     forgotpassword;
     string uid, t_email;
-    if(a==0)
+    if (a == 0)
     {
         cout << " \n\n\n " << ch << ch << " NO RECORD FOUND " << ch << ch << "\n ";
         system("pause");
@@ -227,9 +249,9 @@ void forgotPassword()
     }
     else
     {
-        cout << " \n\n "<< ch << ch << " ENTER USERID -> ";
+        cout << " \n\n " << ch << ch << " ENTER USERID -> ";
         cin >> uid;
-        cout << " \n\n "<< ch << ch << " ENTER EMAIL ID -> ";
+        cout << " \n\n " << ch << ch << " ENTER EMAIL ID -> ";
         cin >> t_email;
     }
 
@@ -237,7 +259,7 @@ void forgotPassword()
     int found = 0;
     for (int k = 0; k < a; k++)
     {
-        if(t_email == R[k].email && uid == R[k].id)
+        if (t_email == R[k].email && uid == R[k].id)
         {
             cout << "\n\n ";
             for (int i = 0; i < 25; i++)
@@ -251,57 +273,55 @@ void forgotPassword()
             break;
         }
     }
-    if(found == 0)
+    if (found == 0)
     {
-        cout << " \n\n " << ch << ch << " USER DATA INCORRECT " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " USER DATA INCORRECT " << ch << ch << "\n ";
         system("pause");
     }
     else
     {
-        cout << " \n\n " << ch << ch << " INFORMATION RETRIEVED SUCCESSFULLY " << ch << ch<<"\n ";
+        cout << " \n\n " << ch << ch << " INFORMATION RETRIEVED SUCCESSFULLY " << ch << ch << "\n ";
         system("pause");
     }
 }
 
-
-
 int main()
 {
-    p:
-        system("cls");
-        cout << endl;
-        heading;
-        cout << "\n\n\n " << ch << ch << " 1.   REGISTER ACCOUNT ";
-        cout << "\n\n " << ch << ch << " 2.   ACCOUNT LOGIN ";
-        cout << "\n\n " << ch << ch << " 3.   FORGOT EMAIL ";
-        cout << "\n\n " << ch << ch << " 4.   FORGOT PASSWORD ";
-        cout << "\n\n " << ch << ch << " 5.   EXIT ";
-        cout << "\n\n ";
-        for (int i = 0; i < 25; i++)
-            cout << ch;
-        int choice;
-        cout << " \n\n ENTER CHOICE -> ";
-        cin >> choice;
+p:
+    system("cls");
+    cout << endl;
+    heading;
+    cout << "\n\n\n " << ch << ch << " 1.   REGISTER ACCOUNT ";
+    cout << "\n\n " << ch << ch << " 2.   ACCOUNT LOGIN ";
+    cout << "\n\n " << ch << ch << " 3.   FORGOT EMAIL ";
+    cout << "\n\n " << ch << ch << " 4.   FORGOT PASSWORD ";
+    cout << "\n\n " << ch << ch << " 5.   EXIT ";
+    cout << "\n\n ";
+    for (int i = 0; i < 25; i++)
+        cout << ch;
+    int choice;
+    cout << " \n\n ENTER CHOICE -> ";
+    cin >> choice;
 
-        switch (choice)
-        {
-            case 1:
-                registerAccount();
-                break;
-            case 2:
-                loginAccount();
-                break;
-            case 3:
-                forgotEmail();
-                break;
-            case 4:
-                forgotPassword();
-                break;
-            case 5:
-                exit(0);
-            default:
-                cout << " \n\n\n " << ch << ch << " NOT VALID CHOICE " << ch << ch << "\n";
-                system("pause");
-        }
+    switch (choice)
+    {
+    case 1:
+        R[a].registerAccount();
+        break;
+    case 2:
+        R[a].loginAccount();
+        break;
+    case 3:
+        R[a].forgotEmail();
+        break;
+    case 4:
+        R[a].forgotPassword();
+        break;
+    case 5:
+        exit(0);
+    default:
+        cout << " \n\n\n " << ch << ch << " NOT VALID CHOICE " << ch << ch << "\n";
+        system("pause");
+    }
     goto p;
 }
