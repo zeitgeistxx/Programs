4c995594d87d33587cc0ac7baf1b2a59c80b03fe
ABHIRUP <abhirupsaha02@gmail.com>
2023-01-20 22:03:56 +0530

    .

--- Sorting Algos/count_sort.cpp
+++ Sorting Algos/count_sort.cpp
@@ -1,55 +1,57 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-int getMax(int a[], int n){
-	int max = INT_MIN; 
-	for(int i=0; i<n; i++){
-		if(a[i] > max){
+int getMax(int a[], int n)
+{
+	int max = INT_MIN;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max)
+		{
 			max = a[i];
 		}
 	}
 	return max;
 }
 
-void countSort(int a[], int n){
+void countSort(int a[], int n)
+{
 	int max = getMax(a, n);
-	int count[max+1] = {0};
+	int count[max + 1] = {0};
 	int output[n];
-	
-	for(int i=0; i<n; i++) count[a[i]]++;
-	
+
+	for (int i = 0; i < n; i++)
+		count[a[i]]++;
+
 	// Relative frequency
-	for(int  i=1; i<=max; i++) count[i]+=count[i-1];
-	
-	for(int i=n-1; i>=0; i--){
+	for (int i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+
+	for (int i = n - 1; i >= 0; i--)
+	{
 		int index = count[a[i]] - 1;
 		count[a[i]]--;
 		output[index] = a[i];
 	}
-	
-	for(int i=0; i<n; i++) a[i] = output[i];
-	
+
+	for (int i = 0; i < n; i++)
+		a[i] = output[i];
 }
 
-
-
-
-
-
-int main(){
+int main()
+{
 	int n;
 	cout << "Enter size of array -> ";
 	cin >> n;
 	int a[n];
-	for(int i=0; i<n; i++){
-		cin >> a[i]; 
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
 	}
-	//cout << getMax(a,n);
-	
-	countSort(a,n);
-	for(int i=0; i<n; i++) cout << a[i] << " "; 
+	// cout << getMax(a,n);
+
+	countSort(a, n);
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
 }
-
-
-
